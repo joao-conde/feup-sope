410f540c054a4b453ed6eb0a80af58db3416c81b
JC13 <joaoconde_13@hotmail.com>
2017-04-17 16:00:42 +0100

    adding more to f5

--- Exercises/f5/p3/p3a.c
+++ Exercises/f5/p3/p3a.c
@@ -4,10 +4,24 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX_NAMES 150
+#define MAX_LINE_SIZE 30
+
+
+int cmp_names(const void* elem1, const void* elem2){
+
+  char firstLetter1 = *((char*)elem1);
+  char firstLetter2 = *((char*)elem2);
+
+  return firstLetter1 - firstLetter2;
+}
+
+
 int main(int argc, char* argv[]){
 
   FILE *fp;
-  char* name_list[150];
+  char* name_list[MAX_NAMES];
+  char* line;
 
   if(argc != 2){
     printf("Wrong format... USAGE: p3a [FILE NAME]\n");
@@ -19,22 +33,31 @@ int main(int argc, char* argv[]){
     exit(2);
   }
 
+
+  for(int j = 0; j < MAX_NAMES; j++){
+    name_list[MAX_NAMES] = NULL;
+  }
+
   int index = 0;
   while(!feof(fp)){
 
-    fgets(fp, &line_size);
-    puts(line_size);
+    line = malloc(sizeof(char) * MAX_LINE_SIZE);
 
+    if(fgets(line,MAX_LINE_SIZE,fp) == NULL)
+      printf("null\n");
+
+    name_list[index] = line;
+    index++;
 
   }
 
-  fclose(fp);
-/*
-  for(int i = 0; i < sizeof(name_list); i++){
-    puts("NAME: ");
-    puts(name_list[i]);
-  }*/
+  //qsort(name_list,sizeof(name_list)/sizeof(line),sizeof(line),cmp_names);
 
+  fclose(fp);
+
+  for(int i=0; name_list[i] != NULL; i++){
+    puts(name_list[i]);
+  }
 
   return 0;
 }
