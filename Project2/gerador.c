8e51ebe6488855594a966711bf758e33de665d22
JC13 <joaoconde_13@hotmail.com>
2017-05-05 19:02:31 +0100

    more changes

--- Project2/gerador.c
+++ Project2/gerador.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 
 int ID = 1;
-
 int GENERATED_M, GENERATED_F, REJECTED_M, REJECTED_F, DISCARDED_M, DISCARDED_F;
+
+char* GENERATE_FIFO = "/tmp/entrada";
+char* REJECTED_FIFO = "/tmp/rejeitados";
 
 typedef struct{
   int id;
@@ -29,18 +35,27 @@ Request* generateRequest(int maxDuration){
 
 int main(int argc, char* argv[]){
 
-  int requests, maxDuration;
-  time_t t;
-
   if(argc != 3){
     printf("Wrong number of arguments. USAGE: program_name <number of requests> <max duration>\n");
     exit(-1);
   }
 
+  if(mkfifo(GENERATE_FIFO, O_RDWR) != 0){
+    printf("Error creating GENERATE fifo\n");
+    exit(-1);
+  }
+
+  int requests = atoi(argv[1]);
+  int maxDuration = atoi(argv[2]);
+  int fifo_fd;
+  time_t t;
   srand((unsigned) time(&t));
 
-  requests = atoi(argv[1]);
-  maxDuration = atoi(argv[2]);
+  if(fifo_fd = open(GENERATE_FIFO, O_WRONLY)) == -1){
+    printf("Error openning for write GENERATE_FIFO\n");
+    exit(-1);
+  }
+
 
 
   Request* r = generateRequest(maxDuration);
@@ -49,6 +64,7 @@ int main(int argc, char* argv[]){
   printf("struct duration: %d\n", r->duration);
   printf("struct denials: %d\n", r->denials);
 
+  /*
   r = generateRequest(maxDuration);
   printf("struct ID: %d\n", r->id);
   printf("struct gender: %s\n", &r->gender);
@@ -72,9 +88,9 @@ int main(int argc, char* argv[]){
   printf("struct gender: %s\n", &r->gender);
   printf("struct duration: %d\n", r->duration);
   printf("struct denials: %d\n", r->denials);
+  */
 
-
-
+  write(fifo_fd,r,sizeof(r));
 
 
   return 0;
--- Project2/sauna.c
+++ Project2/sauna.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int SAUNA_CAPACITY;
-
 int RECEIVED_F, RECEIVED_M, REJECTIONS_F, REJECTIONS_M, SERVED_F, SERVED_M;
+char SAUNA_GENDER;
+
+char* GENERATE_FIFO = "/tmp/entrada";
+char* REJECTED_FIFO = "/tmp/rejeitados";
+
+typedef struct{
+  int id;
+  char gender;
+  int duration;
+  int denials;
+} Request;
 
 int main(int argc, char* argv[]){
+
 
   if(argc != 2){
     printf("Wrong number of arguments. USAGE: program_name <max sauna users>\n");
     exit(-1);
   }
 
+  if(mkfifo(REJECTED_FIFO, O_RDWR) != 0){
+    printf("Error creating REJECTED fifo\n");
+    exit(-1);
+  }
+
+
   SAUNA_CAPACITY = atoi(argv[1]);
+  int fifo_fd;
+  Request* r;
+
+  if((fifo_fd = open(GENERATE_FIFO, O_RDONLY)) == -1){
+    printf("Error openning for read GENERATE_FIFO\n");
+    exit(-1);
+  }
 
 
   printf("number of sauna spots: %d\n", SAUNA_CAPACITY);
+
+  read(fifo_fd, r , sizeof(r));
+
+  printf("id %d\n", r->id);
 
   return 0;
 }
